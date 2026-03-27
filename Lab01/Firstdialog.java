a07421f09ac3ed6589bb70459da31121c8f0b103
unknown <duongngoc3242@gmail.com>
2026-03-27 09:48:05 +0700

    add lab01

---
+++ Lab01/Firstdialog.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class Firstdialog{
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,"Hello world! How are you?");
+        System.exit(0);
+    }
+}
