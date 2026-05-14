14fa1c3756fdfa33dc5910a0afed3110865a95cd
unknown <duongngoc3242@gmail.com>
2026-05-14 19:11:25 +0700

    Thuc hanh so 4

--- Lab03/OtherProjects/Lab01/Firstdialog.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-public class Firstdialog{
-    public static void main(String[] args){
-        JOptionPane.showMessageDialog(null,"Hello world! How are you?");
-        System.exit(0);
-    }
-}
--- Lab03/OtherProjects/Lab01/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Xin chao \n cac ban!");
-        System.out.println("Hello \t world!");
-        }
-}
